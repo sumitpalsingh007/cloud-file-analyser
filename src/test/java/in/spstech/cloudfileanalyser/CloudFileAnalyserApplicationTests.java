b50ff598ed8b38703eff565ff3112d897bd9db3a
sumitpalsingh007 <sumitpalsingh007@gmail.com>
2021-07-11 01:52:25 +0530

    initial commit

---
+++ src/test/java/in/spstech/cloudfileanalyser/CloudFileAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package in.spstech.cloudfileanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
