19c4c956be236e7e4ec14ad6abc1ec7753a3931e
sumitpalsingh007 <sumitpalsingh007@gmail.com>
2021-08-03 11:33:45 +0530

    updating files

--- src/main/java/in/spstech/cloudfileanalyser/JavaHttpClient.java
+++ src/main/java/in/spstech/cloudfileanalyser/JavaHttpClient.java
@@ -21,23 +21,28 @@ import java.util.List;
 import java.util.Map;
 
 public class JavaHttpClient {
+
+    public static final String CLIENT_ID = "8c7a3a91-cc76-48b5-aed2-93e1791682f1";
+    //"a232b40c-6d2a-426a-ba84-38e7fe0b9711";
+    public static final String CLIENT_SECRET = "2vVnbV.Cz._vIbz1isd1_62G3LxGQ-Qqg4";
+    //"em0lVI4-qp41Lan.~z6s.V-X~8eHM49G.g";
+    public static final String USER_NAME = "sumitpal@klassify.org";
+    //"sumitpalsingh007_gmail.com#EXT#@sumitpalsingh007gmail.onmicrosoft.com";
+    public static final String PASSWORD = "IAmEnough@1";
+    public static final String URl = "https://login.microsoftonline.com/adc279f0-657c-4bc8-9e3e-f3665ae24f5b/oauth2/token";
+    //"https://login.microsoftonline.com/sumitpalsingh007gmail.onmicrosoft.com/oauth2/token";
+
     public static void main(String[] args) throws IOException {
         HttpClient httpClient = new DefaultHttpClient();
-        //HttpPost httpPost = new HttpPost("https://login.microsoftonline.com/sumitpalsingh007gmail.onmicrosoft.com/oauth2/token");
-        HttpPost httpPost = new HttpPost("https://login.microsoftonline.com/adc279f0-657c-4bc8-9e3e-f3665ae24f5b/oauth2/token");
-        // Request parameters and other properties.
+        HttpPost httpPost = new HttpPost(URl);
+
         List<NameValuePair> params = new ArrayList<>();
         params.add(new BasicNameValuePair("grant_type", "password"));
-        //params.add(new BasicNameValuePair("client_id", "a232b40c-6d2a-426a-ba84-38e7fe0b9711"));
-        params.add(new BasicNameValuePair("client_id", "8c7a3a91-cc76-48b5-aed2-93e1791682f1"));
-
-        //params.add(new BasicNameValuePair("client_secret", "em0lVI4-qp41Lan.~z6s.V-X~8eHM49G.g"));
-        params.add(new BasicNameValuePair("client_secret", "2vVnbV.Cz._vIbz1isd1_62G3LxGQ-Qqg4"));
-
+        params.add(new BasicNameValuePair("client_id", CLIENT_ID));
+        params.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
         params.add(new BasicNameValuePair("resource", "https://graph.microsoft.com"));
-        //params.add(new BasicNameValuePair("username", "sumitpalsingh007_gmail.com#EXT#@sumitpalsingh007gmail.onmicrosoft.com"));
-        params.add(new BasicNameValuePair("username", "sumitpal@klassify.org"));
-        params.add(new BasicNameValuePair("password", "IAmEnough@1"));
+        params.add(new BasicNameValuePair("username", USER_NAME));
+        params.add(new BasicNameValuePair("password", PASSWORD));
 
         String token = "";
         httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
@@ -46,9 +51,7 @@ public class JavaHttpClient {
         HttpEntity respEntity = response.getEntity();
 
         if (respEntity != null) {
-            // EntityUtils to get the response content
             String content =  EntityUtils.toString(respEntity);
-
             token = String.valueOf(content.split("access_token\":\"")[1]).split("\"")[0];
             System.out.println("Token --" + token);
         }
@@ -56,15 +59,12 @@ public class JavaHttpClient {
         HttpClient httpClient2 = new DefaultHttpClient();
         HttpPost httpPost2 = new HttpPost("https://graph.microsoft.com/v1.0/me/drive/root/children");
 
-        // Request parameters and other properties.
         Map map = new HashMap();
         map.put("Authorization", "Bearer " + token);
-        map.put("client_id", "a232b40c-6d2a-426a-ba84-38e7fe0b9711");
-        //map.put("client_id", "c296f2cb-4380-4df3-873c-b4e22282e649");
+        map.put("client_id", CLIENT_ID);
 
         httpPost2.setHeader("Authorization","Bearer " + token);
-        httpPost2.setHeader("client_id", "a232b40c-6d2a-426a-ba84-38e7fe0b9711");
-        //httpPost2.setHeader("client_id", "c296f2cb-4380-4df3-873c-b4e22282e649");
+        httpPost2.setHeader("client_id", CLIENT_ID);
         httpPost2.setHeader("Content-Type", "application/json");
 
         ObjectMapper om = new ObjectMapper();
@@ -76,7 +76,6 @@ public class JavaHttpClient {
         HttpEntity respEntity2 = response2.getEntity();
 
         if (respEntity2 != null) {
-            // EntityUtils to get the response content
             String content =  EntityUtils.toString(respEntity2);
             System.out.println(content);
         }
